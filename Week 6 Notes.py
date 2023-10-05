e6ac73483fc4eb005df7421ae38d54622ca37d4b
richiespecht <146859525+richiespecht@users.noreply.github.com>
2023-10-05 11:34:47 -0400

    notes

--- Week 6 Notes.py
+++ Week 6 Notes.py
@@ -1,26 +1,39 @@
 # Lists: basically variables with multiple items inside
 
-num_list = [1, 2, 3, 4]
-str_list = ["Mike", "Frog", "Ted", "Cat"]
-mixed_list = [1, "Frog", 2.11, ""]
-empty_list = []
+#num_list = [1, 2, 3, 4]
+#str_list = ["Mike", "Frog", "Ted", "Cat"]
+#mixed_list = [1, "Frog", 2.11, ""]
+#empty_list = []
 
-foo = [1] * 5    # Create a list with a value of 1, and repeat it 5 times
+#foo = [1] * 5    # Create a list with a value of 1, and repeat it 5 times
 
-print (foo)
-print (num_list[0])     # Print the varible in the '0' index
+#print (foo)
+#print (num_list[0])     # Print the varible in the '0' index
 
-boo = str_list[2]       # Set a specific index to a variable
-print (boo)
+#boo = str_list[2]       # Set a specific index to a variable
+#print (boo)
 
-str_list[2] = "Monkey"  # Changing a specific item in a list
-print (str_list)
+#str_list[2] = "Monkey"  # Changing a specific item in a list
+#print (str_list)
 
-str_list.append("Monkey")     # puts an item at the end of a list
-str_list.insert(0, "Monkey")  # inserts a item to the index called for
-str_list.remove("Mike")       # removes an item from the list
-str_list.pop()                # 
-boo = str_list.index("Mike")  # gives the index number of a specific item
-print (len(str_length))       # tells you the length of a list
+#str_list.append("Monkey")     # puts an item at the end of a list
+#str_list.insert(0, "Monkey")  # inserts a item to the index called for
+#str_list.remove("Mike")       # removes an item from the list
+#str_list.pop()                # 
+#boo = str_list.index("Mike")  # gives the index number of a specific item
+#print (len(str_length))       # tells you the length of a list
+
+#for item in str_list:         # prints the individual items of a lise
+#    print (item)
 
 
+num_list = [90,78,81,93]
+
+sum = 0
+for i in num_list:
+    sum += i
+print (sum)
+
+average = sum / len(num_list)
+print ("Average: ", average)
+    

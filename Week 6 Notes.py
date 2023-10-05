249c54cb58bd11724b67ed5e0950e74d8b83a131
richiespecht <146859525+richiespecht@users.noreply.github.com>
2023-10-05 11:13:41 -0400

    Update Week 6 Notes.py

---
+++ Week 6 Notes.py
@@ -0,0 +1,14 @@
+# Lists: basically variables with multiple items inside
+
+num_list = [1, 2, 3, 4]
+str_list = ["Mike", "Frog", "Ted", "Cat"]
+mixed_list = [1, "Frog", 2.11, ""]
+empty_list = []
+
+foo = [1] * 5    # Create a list with a value of 1, and repeat it 5 times
+
+print (foo)
+print (num_list[0])     # Print the varible in the '0' index
+
+boo = str_list[2]       # Set a specific index to a variable
+print (boo)
